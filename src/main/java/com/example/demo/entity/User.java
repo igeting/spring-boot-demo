998505871f51c72b933127478c7f6899575a29e5
c <imcling@qq.com>
2021-05-29 17:21:32 +0800

    user

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,11 +1,10 @@
 package com.example.demo.entity;
 
-import com.example.demo.entity.base.Base;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
-public class User extends Base {
+public class User {
     @SerializedName("name")
     private String username;
     @SerializedName("pass")
--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -3,8 +3,9 @@ package com.example.demo;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -38,6 +40,28 @@ class ApplicationTests {
         Gson gson = new Gson();
         String str = gson.toJson(users);
         log.info(str);
+    }
+
+    @Test
+    void TestGson() {
+        User user = new User();
+        user.setUsername("jack");
+        user.setPassword("123456");
+        //对象转字符串
+        log.info(new Gson().toJson(user));
+        List<User> list = new ArrayList<>();
+        list.add(user);
+        list.add(user);
+        list.add(user);
+        log.info(new Gson().toJson(list));
+
+        //字符串转对象
+        User u = new Gson().fromJson("{\"name\":\"jack\",\"pass\":\"123456\"}", User.class);
+        log.info(u.toString());
+        List<User> us = new Gson().fromJson("[{\"name\":\"jack\",\"pass\":\"123456\"},{\"name\":\"jack\",\"pass\":\"123456\"},{\"name\":\"jack\",\"pass\":\"123456\"}]",
+                new TypeToken<List<User>>() {
+                }.getType());
+        log.info(us.toString());
     }
 
 }
