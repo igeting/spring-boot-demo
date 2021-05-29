993241bcb852cbb6590f0cbea249099d8ddb6820
c <imcling@qq.com>
2021-05-29 17:22:14 +0800

    user

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
