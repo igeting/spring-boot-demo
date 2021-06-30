18d2b1e2bfc48933cd31dcee6eb5b74d8c82cb93
c <imcling@qq.com>
2021-06-30 18:06:07 +0800

    nsq

--- core/src/main/java/com/example/core/config/SystemConfig.java
+++ core/src/main/java/com/example/core/config/SystemConfig.java
@@ -2,11 +2,13 @@ package com.example.core.config;
 
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@ComponentScan(value = {"com.example.nsq"})
 @Configuration
 public class SystemConfig {
     @Bean
