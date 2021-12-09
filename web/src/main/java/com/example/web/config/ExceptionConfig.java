667843cc28873601481b51eaaa500b32202404a7
c <imcling@qq.com>
2021-12-09 10:31:21 +0800

    exception

--- web/src/main/java/com/example/web/config/ExceptionConfig.java
+++ web/src/main/java/com/example/web/config/ExceptionConfig.java
@@ -1,4 +1,4 @@
-package com.example.web.controller;
+package com.example.web.config;
 
 import com.example.web.entity.BaseResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.io.IOException;
 
 @RestControllerAdvice
-public class CommonController {
+public class ExceptionConfig {
 
     @ExceptionHandler(value = Exception.class)
     public BaseResult exceptionHandler(Exception e) {
