8db66cfdb7e7cd016de8c0a989821139e618d623
c <imcling@qq.com>
2021-12-23 11:49:32 +0800

    web

--- web/src/main/java/com/example/web/config/SystemConfig.java
+++ web/src/main/java/com/example/web/config/SystemConfig.java
@@ -3,13 +3,10 @@ package com.example.web.config;
 import org.postgresql.Driver;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
-import java.sql.DriverManager;
 
 @Configuration
 //@ComponentScan(basePackages = {"com.example.web.config"})
@@ -23,13 +20,6 @@ public class SystemConfig {
 
     @Bean(name = "newDataSource")
     public DataSource dataSource() {
-        //DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        //dataSource.setDriverClassName(Driver.class.getName());
-        //dataSource.setUrl("jdbc:postgresql://localhost:5432/test");
-        //dataSource.setUrl("postgres");
-        //dataSource.setPassword("123456");
-        //return dataSource;
-
         return DataSourceBuilder.create()
                 .driverClassName(Driver.class.getName())
                 .url("jdbc:postgresql://localhost:5432/test")
