4abaeb9bdbac8c93847bd6ca25a2647e0c810f46
c <imcling@qq.com>
2022-04-23 16:36:12 +0800

    mybatis plus

--- mybatis/src/main/java/com/example/mybatis/entity/User.java
+++ mybatis/src/main/java/com/example/mybatis/entity/User.java
@@ -1,11 +1,12 @@
 package com.example.mybatis.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "user_info")
 public class User {
     private Long id;
-    private String name;
-    private Integer age;
-    private String email;
+    private String username;
+    private String password;
 }
--- mybatis/src/main/java/com/example/mybatis/mapper/UserMapper.java
+++ mybatis/src/main/java/com/example/mybatis/mapper/UserMapper.java
@@ -2,7 +2,23 @@ package com.example.mybatis.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mybatis.entity.User;
+import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
+//@Mapper
 public interface UserMapper extends BaseMapper<User> {
+    @Select(value = "select * from user_info where username = #{name}")
+    List<User> getUsers(String name);
 
+    @Update(value = "update user_info set username = #{name} where id = #{id}")
+    int updateUser(@Param("id") String id, @Param("name") String username);
+
+    @Insert(value = "insert into user_info (`username`,`password`) values(#{username}, #{password})")
+    int addUser(User user);
+
+    @Delete(value = "delete from user_info where username = #{name}")
+    int delUser(String name);
+
+    User findUser(long id);
 }
