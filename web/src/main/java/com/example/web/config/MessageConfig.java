fe22ba3503e9544ed7920b6aff27e2c748196ee0
c <imcling@qq.com>
2022-04-29 14:20:04 +0800

    swagger

--- web/src/main/java/com/example/web/config/MessageConfig.java
+++ web/src/main/java/com/example/web/config/MessageConfig.java
@@ -1,21 +1,29 @@
 package com.example.web.config;
 
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import springfox.documentation.spring.web.json.Json;
+
+import java.lang.reflect.Type;
 
 @Configuration
-public class MessageConfig {
+public class MessageConfig implements JsonSerializer<Json> {
+
+    @Override
+    public JsonElement serialize(Json src, Type typeOfSrc, JsonSerializationContext context) {
+        return JsonParser.parseString(src.value());
+    }
 
     @Bean
     public HttpMessageConverters httpMessageConverters() {
         GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
         converter.setGson(new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .registerTypeAdapter(Json.class, new MessageConfig())
                 .create());
         return new HttpMessageConverters(converter);
     }
-
 }
