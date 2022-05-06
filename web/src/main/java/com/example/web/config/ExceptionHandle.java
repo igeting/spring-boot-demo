5a81367465d68428de0a92e8b77aa39b6b57e092
c <imcling@qq.com>
2022-05-06 16:21:40 +0800

    validation

--- web/src/main/java/com/example/web/config/ExceptionHandle.java
+++ web/src/main/java/com/example/web/config/ExceptionHandle.java
@@ -2,15 +2,19 @@ package com.example.web.config;
 
 import com.example.web.entity.BaseResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandle {
+public class ExceptionHandle {
 
     @ExceptionHandler(value = Exception.class)
-    public BaseResult exceptionHandle(Exception e) {
+    public BaseResult exceptionHandle(Exception e, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return BaseResult.fail(bindingResult.getFieldError().getDefaultMessage(), ResultEnum.FAIL.getCode());
+        }
         log.error("ExceptionHandle:{}", e.getMessage(), e);
         return BaseResult.fail(e.getMessage(), ResultEnum.FAIL.getCode());
     }
