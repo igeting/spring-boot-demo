cff8c8858f9d44acd15a4f88c2fd31cfa1909cd1
c <imcling@qq.com>
2022-05-09 20:02:35 +0800

    error

--- web/src/main/java/com/example/web/config/ExceptionHandle.java
+++ web/src/main/java/com/example/web/config/ExceptionHandle.java
@@ -1,21 +1,24 @@
 package com.example.web.config;
 
 import com.example.web.model.base.BaseResult;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class ExceptionHandle {
 
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public BaseResult exceptionHandle(Exception e, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return BaseResult.fail(bindingResult.getFieldError().getDefaultMessage(), ResultEnum.FAIL.getCode());
         }
-        log.error("ExceptionHandle:{}", e.getMessage(), e);
+        return BaseResult.fail(e.getMessage(), ResultEnum.FAIL.getCode());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public BaseResult exceptionHandle(Exception e) {
         return BaseResult.fail(e.getMessage(), ResultEnum.FAIL.getCode());
     }
 }
